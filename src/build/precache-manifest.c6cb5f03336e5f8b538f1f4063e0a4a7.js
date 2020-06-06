44756924425b3f9461a46fd0eca1bbbfb4b054a7
Jonathan Mortimer <jonathan@jonathans-mbp.home>
2020-06-06 13:25:53 +0100

    First commit

---
+++ src/build/precache-manifest.c6cb5f03336e5f8b538f1f4063e0a4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef39eba6be72f152e4c3c2e3746c5701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89555b6f93395386eae",
+    "url": "/static/css/main.016fa8d6.chunk.css"
+  },
+  {
+    "revision": "8148389af6e6c395e1e8",
+    "url": "/static/js/2.559f4170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.559f4170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89555b6f93395386eae",
+    "url": "/static/js/main.a03fd084.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
